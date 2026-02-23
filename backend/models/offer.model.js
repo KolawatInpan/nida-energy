304826c1b879b7e43663a4a369766be940ff7f24
KolawatInpan <dominolity@gmail.com>
2026-02-23 22:53:34 +0700

    offer

--- backend/models/offer.model.js
+++ backend/models/offer.model.js
@@ -1,15 +1,16 @@
 const { PrismaClient } = require('@prisma/client');
+const WalletModel = require('./wallet.model');
+const BuildingModel = require('./building.model');
 const prisma = new PrismaClient();
 
 async function getOffers() {
-    return await prisma.energyOffer.findMany({ 
-        include: { market: true } });
+    return await prisma.energyOffer.findMany();
 }
 
 async function getAvailableOffers() {
-    return await prisma.energyOffer.findMany({ 
-        where: { status: 'AVAILABLE' }, 
-        include: { market: true }});
+    return await prisma.energyOffer.findMany({
+        where: { status: 'AVAILABLE'}
+    })
 }
 
 async function getSoldOffers() {
@@ -24,9 +25,20 @@ async function getOfferById(id) {
         include: { market: true }});
 }
 
+async function getBuildingByWalletId(walletId) {
+    // sellerwallet and building link with email
+    const wallet = await WalletModel.getWalletById(walletId);
+    if (!wallet) {
+        throw new Error('Wallet not found');
+    }
+    const building = await BuildingModel.getBuilding(wallet.id);
+    return building;
+}
+
 module.exports = {
     getOffers,
     getAvailableOffers,
     getSoldOffers,
-    getOfferById
+    getOfferById,
+    getBuildingByWalletId
 }
